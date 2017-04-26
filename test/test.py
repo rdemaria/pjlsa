6655e6d916b61ef5e152018599d5956b97c73230
Riccardo De Maria <riccardodemaria@gmail.com>
2017-04-26 15:00:20 +0200

    updating test

--- test/test.py
+++ test/test.py
@@ -26,4 +26,16 @@ pl=lsa.getParameterList('LHCBEAM1')
 pgl=list(lsa.parameterService.findParameterGroupsByAccelerator(bp.getAccelerator()))
 
 
+t1='2015-11-22 00:00:00'
+t2='2015-11-23 00:00:00'
+trims = lsa.getTrims(beamprocess="PHYSICS-2.51TeV-4m-2015_V1@90_[END]", parameter="LHCBEAM2/IP1_SEPSCAN_Y_MM", start=t1, end=t2)
+
+
+trims = lsa.getTrims(beamprocess="PHYSICS-2.51TeV-4m-2015_V1@90_[END]",        parameter="LHCBEAM2/IP1_SEPSCAN_Y_MM", start=t1, end=t2)
+
+lsa.getOpticTable('RAMP-SQUEEZE-6.5TeV-ATS-1m-2017_V1')
+
+ot = lsa.getOpticTable('PHYSICS-2.51TeV-4m-2015_V1@90_[END]')
+f = lsa.getKnobFactors('LHCBEAM2/IP1_SEPSCAN_Y_MM', ot[0])
+
 
