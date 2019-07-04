7f3d0a65f3c8e600aac8d9271edf1ce0fbe71f44
Michi <michi@cern.ch>
2019-07-04 21:11:03 +0200

    trim service

--- pjlsa/service/trim_service.py
+++ pjlsa/service/trim_service.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from typing import Iterable, Union
+from typing import List, Iterable, Union, Optional, Mapping, Any
 
 from .. import _jpype as _jp
 from ..domain import *
@@ -11,7 +11,11 @@ class LsaTrimService(object):
     def __init__(self, lsa_client):
         self._lsa = lsa_client
 
-    def trimSettings(self):
+    def trimSettings(self, *, context: Union[str, Context], settings: Mapping[Union[str, Parameter], Any],
+                     settingPart: Optional[SettingPartEnum] = None, relative: bool = False, drive: bool = True,
+                     persist: bool = True, propagateToChildren: bool = True, ignoreErrors: bool = False,
+                     lenientDrive: bool = False, commit: bool = True, forceDrive: bool = False,
+                     description: Optional[str] = None, attributes: Optional[Mapping[str, str]] = None):
         pass
 
     def revertTrim(self):
@@ -31,7 +35,7 @@ class LsaTrimService(object):
 
     def findTrimHeaders(self, contexts: Union[str, Iterable[str], Context, Iterable[Context]],
                         parameters: Union[str, Iterable[str], Parameter, Iterable[Parameter]], *,
-                        since: Union[None, datetime, int, str] = None):
+                        since: Union[None, datetime, int, str] = None) -> List[TrimHeader]:
         resolved_contexts = self._lsa.contextService._resolve_contexts(contexts)
         beamprocesses = [bp for ctx in resolved_contexts for bp in ctx.beamProcesses]
         builder = _jp.cern.lsa.domain.settings.TrimHeadersRequest.builder()
