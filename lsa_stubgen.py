3feacf224a9e3caf7d1d2fef00009b93c5289ce3
mihostet <michi.hostettler@cern.ch>
2020-05-04 13:17:08 +0200

    bump version

--- lsa_stubgen.py
+++ lsa_stubgen.py
@@ -9,5 +9,5 @@ prefixes = ["java",
             "cern.accsoft.commons",
             "cern.rbac.common",
             "cern.japc.core", "cern.japc.value"]
-#prefixes = ["java.util"]
+
 stubgenj.generate_java_stubs(prefixes, use_stubs_suffix=True, output_dir='.')
--- pjlsa/__init__.py
+++ pjlsa/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.0.16"
+__version__ = "0.1.0"
 
 __cmmnbuild_deps__ = [
     "log4j",
